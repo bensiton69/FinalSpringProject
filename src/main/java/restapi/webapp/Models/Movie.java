50b9f368455741a91dff24192a39909fe6e3a249
Ben Israel Siton <benisraels@my.hit.ac.il>
2022-06-20 19:04:37 +0300

    adding genres to movie as string array

--- src/main/java/restapi/webapp/Models/Movie.java
+++ src/main/java/restapi/webapp/Models/Movie.java
@@ -4,11 +4,13 @@ package restapi.webapp.Models;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import net.minidev.json.annotate.JsonIgnore;
 import restapi.webapp.Enums.eGenre;
 //import restapi.webapp.Enums.eGenre;
 
 import javax.persistence.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Data
@@ -24,8 +26,12 @@ public class Movie {
     private int duration;
     private int yearOfPublish;
 
+    //TODO: Transfer to enum
+    private String[] genres;
+
+//    @JsonIgnore
 //    @Enumerated(EnumType.STRING)
-//    private eGenre[] genre = new eGenre[]();
+//    private eGenre[] genre;
 
     @OneToMany
     private List<ShowTime> showTimes = new ArrayList<ShowTime>();
@@ -39,6 +45,7 @@ public class Movie {
                 ", link='" + link + '\'' +
                 ", duration=" + duration +
                 ", yearOfPublish=" + yearOfPublish +
+                ", genres=" + Arrays.toString(genres) +
                 ", showTimes=" + showTimes +
                 '}';
     }
