ab672f45a6802f76b0cb49ee8accf3ef395fe540
Ben Israel Siton <benisraels@my.hit.ac.il>
2022-06-20 14:11:52 +0300

    MovieEntityFactory init and MoviesController modified

--- src/main/java/restapi/webapp/Factories/MovieEntityFactory.java
+++ src/main/java/restapi/webapp/Factories/MovieEntityFactory.java
@@ -1,11 +1,32 @@
 package restapi.webapp.Factories;
 
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.stereotype.Component;
 import restapi.webapp.Models.Movie;
 import restapi.webapp.SimpleIdentifiableRepresentationModelAssembler;
+import org.springframework.hateoas.server.SimpleRepresentationModelAssembler;
 import restapi.webapp.controllers.MoviesController;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Component
-public class MovieEntityFactory extends SimpleIdentifiableRepresentationModelAssembler<Movie> {
-    public MovieEntityFactory(){super(MoviesController.class);}
+public class MovieEntityFactory implements SimpleRepresentationModelAssembler<Movie> {
+    @Override
+    public void addLinks(EntityModel<Movie> resource) {
+        resource.add(
+                linkTo(methodOn(MoviesController.class).getMoviesById(resource.getContent().getId()))
+                        .withSelfRel());
+
+        resource.add(
+                linkTo(methodOn(MoviesController.class).getMoviesAsResponseEntity())
+                        .withRel("Movies info")
+        );
+    }
+
+    @Override
+    public void addLinks(CollectionModel<EntityModel<Movie>> resources) {
+        resources.add(linkTo(methodOn(MoviesController.class).getMoviesAsResponseEntity()).withSelfRel());
+
+    }
 }
--- src/main/java/restapi/webapp/controllers/MoviesController.java
+++ src/main/java/restapi/webapp/controllers/MoviesController.java
@@ -6,9 +6,11 @@ import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import restapi.webapp.Factories.MovieEntityFactory;
 import restapi.webapp.Models.Movie;
+import restapi.webapp.Post;
 import restapi.webapp.Repositories.MovieRepos;
 import restapi.webapp.Services.MovieService;
 
@@ -29,21 +31,21 @@ public class MoviesController {
         this.movieEntityFactory = movieEntityFactory;
     }
 
-//    @GetMapping("Movies")
-//    public CompletableFuture<Movie> getAllMoviesFromService(){
-//        logger.info(movieService.Movies().toString());
-//        return(movieService.Movies());
-//    }
+    @GetMapping("/Movies/{id}")
+    public ResponseEntity<EntityModel<Movie>> getMoviesById(@PathVariable Long id) {
+        return  movieRepos.findById(id)
+                .map(movieEntityFactory::toModel)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
 
-
-//    @GetMapping("/MoviesNOTFINISHED")
-//    public ResponseEntity<CollectionModel<EntityModel<Movie>>> profilePosts() {
-//        return ResponseEntity.ok(
-//                movieEntityFactory.toCollectionModel(movieRepos.findAll()));
-//    }
-
-    @GetMapping("/Movies")
-    public List<Movie> profilePosts() {
+    @GetMapping("/MoviesAsList")
+    public List<Movie> getMoviesAsList() {
         return (List<Movie>) movieRepos.findAll();
+    }
+
+    @GetMapping("/MoviesAsResponseEntity")
+    public ResponseEntity<CollectionModel<EntityModel<Movie>>> getMoviesAsResponseEntity() {
+        return ResponseEntity.ok(movieEntityFactory.toCollectionModel(movieRepos.findAll()));
     }
 }
